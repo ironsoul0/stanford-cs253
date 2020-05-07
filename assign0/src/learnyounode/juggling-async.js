56eeacb88d7408003a967c5b9d16c4adab58b867
ironsoul0 <timka2609@gmail.com>
2020-05-07 14:45:17 +0600

    more tasks

--- assign0/src/learnyounode/juggling-async.js
+++ assign0/src/learnyounode/juggling-async.js
@@ -1,29 +1,28 @@
 const http = require('http')
 
-const urls = [], contents = []
+const urls = []; const contents = []
 
 for (let i = 2; i < process.argv.length; i++) {
-    urls.push(process.argv[i])
-    contents.push(null)
+  urls.push(process.argv[i])
+  contents.push(null)
 }
 
 let ended = 0
 
 for (let i = 0; i < urls.length; i++) {
-    http.get(urls[i], (response) => {
-        let data = ''
+  http.get(urls[i], (response) => {
+    let data = ''
 
-        response.on('data', chunk => {
-            data += chunk
-        })
+    response.on('data', chunk => {
+      data += chunk
+    })
 
-        response.on('end', () => {
-            contents[i] = data
-            ended++
-            if (ended == urls.length) {
-                for (let j = 0; j < ended; j++)
-                    console.log(contents[j])
-            }
-        })
-    });
+    response.on('end', () => {
+      contents[i] = data
+      ended++
+      if (ended === urls.length) {
+        for (let j = 0; j < ended; j++) { console.log(contents[j]) }
+      }
+    })
+  })
 }
--- assign0/src/learnyounode/time-server.js
+++ assign0/src/learnyounode/time-server.js
@@ -1,1 +1,18 @@
-// TODO: Replace this with your solution.
+const net = require('net')
+
+const createDate = () => {
+  const myTimezone = new Date().toLocaleString('en-US', {
+    timeZone: 'Asia/Almaty'
+  })
+  const now = new Date(myTimezone)
+  const current = now.toISOString().replace(/T/, ' ').replace(/\..+/, '')
+  return current.substring(0, current.length - 3)
+}
+
+const server = net.createServer(socket => {
+  socket.write(createDate())
+  socket.pipe(socket)
+  socket.destroy()
+})
+
+server.listen(process.argv[2], '127.0.0.1')
