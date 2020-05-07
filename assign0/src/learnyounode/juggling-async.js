18d43e2627f021a24b6fb884b4eaa6eabb9a5e01
ironsoul0 <timka2609@gmail.com>
2020-05-07 14:16:33 +0600

    jugglin is good

--- assign0/src/learnyounode/juggling-async.js
+++ assign0/src/learnyounode/juggling-async.js
@@ -1,1 +1,29 @@
-// TODO: Replace this with your solution.
+const http = require('http')
+
+const urls = [], contents = []
+
+for (let i = 2; i < process.argv.length; i++) {
+    urls.push(process.argv[i])
+    contents.push(null)
+}
+
+let ended = 0
+
+for (let i = 0; i < urls.length; i++) {
+    http.get(urls[i], (response) => {
+        let data = ''
+
+        response.on('data', chunk => {
+            data += chunk
+        })
+
+        response.on('end', () => {
+            contents[i] = data
+            ended++
+            if (ended == urls.length) {
+                for (let j = 0; j < ended; j++)
+                    console.log(contents[j])
+            }
+        })
+    });
+}
