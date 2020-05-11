f6a3160c7c99907f2a6b89b249ea9d8e930ecc50
ironsoul0 <timka2609@gmail.com>
2020-05-11 16:54:45 +0600

    lec7

---
+++ lec7/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const ejs = require('ejs')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    const { name } = req.query || 'unnamed person'
+    const template = `
+        <h1>Hi, <%= name %>.</h1>
+    `
+    const html = ejs.render(template, { name: name })
+    res.send(html)
+})
+
+app.listen(4000)
